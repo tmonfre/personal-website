e41447134895ee724b888e9b7d90482e17a5081a
Thomas Monfre <tmonfre1@gmail.com>
2021-12-29 10:09:28 -0600

    feat: footer icons + resume link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,20 @@
 import './styles.scss';
 
+import GitHubIcon from '../../assets/icons/github.png';
+import LinkedInIcon from '../../assets/icons/linkedin.png';
+import EmailIcon from '../../assets/icons/mail.png';
+
 const Footer = (): JSX.Element => (
   <footer>
-    <a href="https://github.com/tmonfre" target="_blank" rel="noreferrer">GitHub</a>
-    <a href="https://twitter.com/thomasmonfre" target="_blank" rel="noreferrer">Twitter</a>
-    <a href="https://www.linkedin.com/in/thomas-monfre/" target="_blank" rel="noreferrer">LinkedIn</a>
-    <a href="mailto:tmonfre1@gmail.com">Email</a>
+    <a href="https://github.com/tmonfre" target="_blank" rel="noreferrer">
+      <img src={GitHubIcon} alt="github" />
+    </a>
+    <a href="https://www.linkedin.com/in/thomas-monfre/" target="_blank" rel="noreferrer">
+      <img src={LinkedInIcon} alt="linkedin" />
+    </a>
+    <a href="mailto:tmonfre1@gmail.com">
+      <img src={EmailIcon} alt="email" />
+    </a>
   </footer>
 );
 
--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,11 +1,15 @@
 import './styles.scss';
 
-import ProfileImage from '../../assets/profile.png';
+import { PageRoutes } from '../../constants';
+
+import ProfileImage from '../../assets/images/profile.png';
 
 const About = (): JSX.Element => (
   <div id="about-container">
     <p>About</p>
     <img src={ProfileImage} alt="me" />
+    {/* note this doesn't work in dev environment */}
+    <a href={window.location.origin + PageRoutes.Resume} target="_blank" rel="noreferrer">Resume</a>
   </div>
 );
 
