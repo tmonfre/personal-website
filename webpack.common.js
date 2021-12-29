f5a7f5d10ee384408fe8899ef42e726fa2f038cd
Thomas Monfre <tmonfre1@gmail.com>
2021-12-28 23:07:33 -0600

    feat: add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -33,6 +33,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
+      favicon: path.join(__dirname, 'src', 'favicon.ico'),
     }),
   ],
 };
