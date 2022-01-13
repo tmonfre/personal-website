4d72be477c477b1cd964e89e9298719bddb70f17
Thomas Monfre <tmonfre1@gmail.com>
2022-01-13 11:37:29 -0600

    feat: fetch zoom.tar.gz via GitHub API latest release

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const express = require('express');
+const axios = require('axios');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -9,8 +10,10 @@ const app = express();
 app.use(express.static(`${__dirname}/build`));
 app.use(express.static(`${__dirname}/static`));
 
-app.get('/resume', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/resume.pdf'));
+app.get('/zoom.tar.gz', async (req, res) => {
+  const { data } = await axios.get('https://api.github.com/repos/tmonfre/zoom-cli/releases/latest');
+  const { browser_download_url: redirectUrl } = data.assets.find((asset) => asset.name === 'zoom.tar.gz');
+  res.redirect(redirectUrl);
 });
 
 app.get('*', (req, res) => {
